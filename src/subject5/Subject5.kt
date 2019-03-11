138dc0cae050cd289602b825e30be0f70a39d018
personal_cy@163.com <aimin12131028>
2019-03-11 17:16:28 +0800

    subject5 最长回文串  添加说明

--- src/subject5/Subject5.kt
+++ src/subject5/Subject5.kt
@@ -3,34 +3,19 @@ package subject5
 import subject3.lengthOfLongestSubstring
 
 /**
- * 6. Z字形变换
+ * 6. 最长回文子串
  *
- * 将字符串 "PAYPALISHIRING" 以Z字形排列成给定的行数：
- *     P   A   H   N
- *     A P L S I I G
- *     Y   I   R
- * 之后从左往右，逐行读取字符："PAHNAPLSIIGYIR"
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
  *
- * 实现一个将字符串进行指定行数变换的函数:
+ * 示例 1：
  *
- * string convert(string s, int numRows);
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
  *
- * 示例 1:
- *
- *      输入: s = "PAYPALISHIRING", numRows = 3
- *      输出: "PAHNAPLSIIGYIR"
- *
- * 示例 2:
- *
- *      输入: s = "PAYPALISHIRING", numRows = 4
- *      输出: "PINALSIGYAHRPI"
- *
- *      解释:
- *
- *          P     I    N
- *          A   L S  I G
- *          Y A   H R
- *          P     I
+ * 输入: "cbbd"
+ * 输出: "bb"
  */
 fun main(args: Array<String>) {
     println(longestPalindrome("PAYPALISHIRING"))
